14cc1b905cc7807a076af0969b6d5367e3ad0ed8
Adem Maroua <mn.adem@esi-sba.dz>
2023-12-16 08:22:10 +0100

    Home section

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,19 @@ module.exports = {
         redColor: "#cc3433",
         darkColor: '#000',
         darkColorLight: "#171717",
+      },
+      keyframes: {
+        move: {
+          "50%": {
+            transform: 'translateY(-1rem)'
+          }
+        }
+      },
+      
+      animation: {
+        'movingY': 'move 2s linear infinite'
       }
+      
     },
     container:{
       center:true,
